86e6e79ffaab9f4827d8d9f5974d836e06e653e2
cocopon <cocopon@me.com>
2014-10-10 14:48:37 +0900

    Change output format

--- CCPClassDump/CCPClassDump/Formatter/CCPClassDumpDefaultResultFormatter.m
+++ CCPClassDump/CCPClassDump/Formatter/CCPClassDumpDefaultResultFormatter.m
@@ -101,7 +101,7 @@ static NSString *const kMethodSeparator = @":";
 
 
 - (NSString*)stringFromProperty:(CCPProperty*)property {
-    return [NSString stringWithFormat:@"@property (?) ? %@;\n",
+    return [NSString stringWithFormat:@"@property %@;\n",
             [property name]];
 }
 
